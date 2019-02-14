7a90e7c50b772e89f376d3e69b6da4db64d81b9b
biharck <biharck@gmail.com>
2019-02-14 06:28:59 -0300

    adding rule to do not include this ContentSequence if it uses 3D format

--- src/utilities/TID300/Polyline.js
+++ src/utilities/TID300/Polyline.js
@@ -5,16 +5,19 @@ import TID300Measurement from "./TID300Measurement.js";
  * Expand an array of points stored as objects into
  * a flattened array of points
  *
- * @param points [{x: 0, y: 1}, {x: 1, y: 2}]
- * @return {Array} [point1x, point1y, point2x, point2y]
+ * @param points [{x: 0, y: 1}, {x: 1, y: 2}] or [{x: 0, y: 1, z: 0}, {x: 1, y: 2, z: 0}]
+ * @return {Array} [point1x, point1y, point2x, point2y] or [point1x, point1y, point1z, point2x, point2y, point2z]
  */
 function expandPoints(points) {
     const allPoints = [];
 
-    points.forEach(point => {
-        allPoints.push(point.x);
-        allPoints.push(point.y);
-    });
+  points.forEach(point => {
+    allPoints.push(point.x);
+    allPoints.push(point.y);
+    if(point.z){
+      allPoints.push(point.z)
+    }
+  });
 
     return allPoints;
 }
@@ -41,100 +44,99 @@ export default class Polyline extends TID300Measurement {
 
         // TODO: Add Mean and STDev value of (modality?) pixels
 
-        return [
-            {
-                RelationshipType: "HAS OBS CONTEXT",
-                ValueType: "TEXT",
-                ConceptNameCodeSequence: {
-                    CodeValue: "112039",
-                    CodingSchemeDesignator: "DCM",
-                    CodeMeaning: "Tracking Identifier"
-                },
-                TextValue: "web annotation"
-            },
-            {
-                RelationshipType: "HAS OBS CONTEXT",
-                ValueType: "UIDREF",
-                ConceptNameCodeSequence: {
-                    CodeValue: "112040",
-                    CodingSchemeDesignator: "DCM",
-                    CodeMeaning: "Tracking Unique Identifier"
-                },
-                UID: DicomMetaDictionary.uid()
-            },
-            {
-                RelationshipType: "CONTAINS",
-                ValueType: "CODE",
-                ConceptNameCodeSequence: {
-                    CodeValue: "121071",
-                    CodingSchemeDesignator: "DCM",
-                    CodeMeaning: "Finding"
-                },
-                ConceptCodeSequence: {
-                    CodeValue: "SAMPLEFINDING",
-                    CodingSchemeDesignator: "99dcmjs",
-                    CodeMeaning: "Sample Finding"
-                }
-            },
-            {
-                RelationshipType: "CONTAINS",
-                ValueType: "NUM",
-                ConceptNameCodeSequence: {
-                    CodeValue: "G-A197",
-                    CodingSchemeDesignator: "SRT",
-                    CodeMeaning: "Perimeter" // TODO: Look this up from a Code Meaning dictionary
-                },
-                MeasuredValueSequence: {
-                    MeasurementUnitsCodeSequence: {
-                        CodeValue: "mm",
-                        CodingSchemeDesignator: "UCUM",
-                        CodingSchemeVersion: "1.4",
-                        CodeMeaning: "millimeter"
-                    },
-                    NumericValue: perimeter
-                },
-                ContentSequence: {
-                    RelationshipType: "INFERRED FROM",
-                    ValueType: "SCOORD",
-                    GraphicType: "POLYLINE",
-                    GraphicData,
-                    ContentSequence: {
-                        RelationshipType: "SELECTED FROM",
-                        ValueType: "IMAGE",
-                        ReferencedSOPSequence
-                    }
-                }
-            },
-            {
-                // TODO: This feels weird to repeat the GraphicData
-                RelationshipType: "CONTAINS",
-                ValueType: "NUM",
-                ConceptNameCodeSequence: {
-                    CodeValue: "G-A166",
-                    CodingSchemeDesignator: "SRT",
-                    CodeMeaning: "Area" // TODO: Look this up from a Code Meaning dictionary
-                },
-                MeasuredValueSequence: {
-                    MeasurementUnitsCodeSequence: {
-                        CodeValue: "mm2",
-                        CodingSchemeDesignator: "UCUM",
-                        CodingSchemeVersion: "1.4",
-                        CodeMeaning: "SquareMilliMeter"
-                    },
-                    NumericValue: perimeter
-                },
-                ContentSequence: {
-                    RelationshipType: "INFERRED FROM",
-                    ValueType: "SCOORD",
-                    GraphicType: "POLYLINE",
-                    GraphicData,
-                    ContentSequence: {
-                        RelationshipType: "SELECTED FROM",
-                        ValueType: "IMAGE",
-                        ReferencedSOPSequence
-                    }
-                }
-            }
-        ];
-    }
+    return [
+      {
+        RelationshipType: 'HAS OBS CONTEXT',
+        ValueType: 'TEXT',
+        ConceptNameCodeSequence: {
+          CodeValue: '112039',
+          CodingSchemeDesignator: 'DCM',
+          CodeMeaning: 'Tracking Identifier',
+        },
+        TextValue: 'web annotation',
+      },
+      {
+        RelationshipType: 'HAS OBS CONTEXT',
+        ValueType: 'UIDREF',
+        ConceptNameCodeSequence: {
+          CodeValue: '112040',
+          CodingSchemeDesignator: 'DCM',
+          CodeMeaning: 'Tracking Unique Identifier',
+        },
+        UID: DicomMetaDictionary.uid(),
+      },
+      {
+        RelationshipType: 'CONTAINS',
+        ValueType: 'CODE',
+        ConceptNameCodeSequence: {
+          CodeValue: '121071',
+          CodingSchemeDesignator: 'DCM',
+          CodeMeaning: 'Finding',
+        },
+        ConceptCodeSequence: {
+          CodeValue: 'SAMPLEFINDING',
+          CodingSchemeDesignator: '99dcmjs',
+          CodeMeaning: 'Sample Finding',
+        },
+      },
+      {
+        RelationshipType: 'CONTAINS',
+        ValueType: 'NUM',
+        ConceptNameCodeSequence: {
+          CodeValue: 'G-A197',
+          CodingSchemeDesignator: 'SRT',
+          CodeMeaning: 'Perimeter', // TODO: Look this up from a Code Meaning dictionary
+        },
+        MeasuredValueSequence: {
+          MeasurementUnitsCodeSequence: {
+            CodeValue: 'mm',
+            CodingSchemeDesignator: 'UCUM',
+            CodingSchemeVersion: '1.4',
+            CodeMeaning: 'millimeter',
+          },
+          NumericValue: perimeter,
+        },
+        ContentSequence: use3DSpatialCoordinates ? undefined :{
+          RelationshipType: 'INFERRED FROM',
+          ValueType: 'SCOORD',
+          GraphicType: 'POLYLINE',
+          GraphicData,
+          ContentSequence: {
+            RelationshipType: 'SELECTED FROM',
+            ValueType: 'IMAGE',
+            ReferencedSOPSequence
+          },
+        },
+      },
+      { // TODO: This feels weird to repeat the GraphicData
+        RelationshipType: 'CONTAINS',
+        ValueType: 'NUM',
+        ConceptNameCodeSequence: {
+          CodeValue: 'G-A166',
+          CodingSchemeDesignator: 'SRT',
+          CodeMeaning: 'Area', // TODO: Look this up from a Code Meaning dictionary
+        },
+        MeasuredValueSequence: {
+          MeasurementUnitsCodeSequence: {
+            CodeValue: 'mm2',
+            CodingSchemeDesignator: 'UCUM',
+            CodingSchemeVersion: '1.4',
+            CodeMeaning: 'SquareMilliMeter',
+          },
+          NumericValue: perimeter,
+        },
+        ContentSequence: use3DSpatialCoordinates ? undefined : {
+          RelationshipType: 'INFERRED FROM',
+          ValueType: 'SCOORD',
+          GraphicType: 'POLYLINE',
+          GraphicData,
+          ContentSequence: {
+            RelationshipType: 'SELECTED FROM',
+            ValueType: 'IMAGE',
+            ReferencedSOPSequence
+          },
+        },
+      },
+    ];
+  };
 }
