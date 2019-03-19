60ce4698366b93d4802defd0d5208837e1d7fbb5
biharck <biharck@gmail.com>
2019-03-18 22:39:55 -0300

    removing expandPoints for points

--- src/utilities/TID300/Point.js
+++ src/utilities/TID300/Point.js
@@ -1,27 +1,6 @@
 import { DicomMetaDictionary } from "../../DicomMetaDictionary.js";
 import TID300Measurement from "./TID300Measurement.js";
 
-/**
- * Expand an array of points stored as objects into
- * a flattened array of points
- *
- * @param points [{x: 0, y: 1}, {x: 1, y: 2}] or [{x: 0, y: 1, z: 0}, {x: 1, y: 2, z: 0}]
- * @return {Array} [point1x, point1y, point2x, point2y] or [point1x, point1y, point1z, point2x, point2y, point2z]
- */
-function expandPoints(points) {
-    const allPoints = [];
-
-    points.forEach(point => {
-        allPoints.push(point[0]);
-        allPoints.push(point[1]);
-        if (point[2] !== undefined) {
-            allPoints.push(point[2]);
-        }
-    });
-
-    return allPoints;
-}
-
 export default class Point extends TID300Measurement {
     // Note: the last point should be equal to the first point to indicate that the point is closed.
     constructor({
@@ -51,7 +30,7 @@ export default class Point extends TID300Measurement {
         // const reducer = (accumulator, currentValue) => accumulator + currentValue;
         // const perimeter = lengths.reduce(reducer);
         const perimeter = {};
-        const GraphicData = expandPoints(points);
+        const GraphicData = points[0];
 
         // TODO: Add Mean and STDev value of (modality?) pixels
 
