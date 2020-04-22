bd84069a10bfccec84e2f724adcef999ab3c095d
igoroctaviano <igoroctaviano@gmail.com>
2020-04-22 10:38:02 -0300

    Improve for loops

--- src/adapters/Cornerstone/Segmentation_4X.js
+++ src/adapters/Cornerstone/Segmentation_4X.js
@@ -340,7 +340,11 @@ function generateToolState(imageIds, arrayBuffer, metadataProvider) {
 
     let inPlane = true;
 
-    for (let i = 0; i < PerFrameFunctionalGroupsSequence.length; i++) {
+    for (
+        let i = 0, groupsLen = PerFrameFunctionalGroupsSequence.length;
+        i < groupsLen;
+        ++i
+    ) {
         const PerFrameFunctionalGroups = PerFrameFunctionalGroupsSequence[i];
 
         const ImageOrientationPatientI =
@@ -408,20 +412,22 @@ function generateToolState(imageIds, arrayBuffer, metadataProvider) {
 
         const data = alignedPixelDataI.data;
 
-        let frameHasSegments = false;
-        for (let j = 0; j < alignedPixelDataI.data.length; j++) {
+        for (let j = 0, len = alignedPixelDataI.data.length; j < len; ++j) {
             if (data[j]) {
-                frameHasSegments = true;
-                labelmap2DView[j] = segmentIndex;
+                for (let x = j + 1; x < len; ++x) {
+                    if (data[x]) {
+                        labelmap2DView[x] = segmentIndex;
+                    }
+                }
+
+                if (!segmentsOnFrame[imageIdIndex]) {
+                    segmentsOnFrame[imageIdIndex] = [];
+                }
+
+                segmentsOnFrame[imageIdIndex].push(segmentIndex);
+
+                break;
             }
-        }
-
-        if (!segmentsOnFrame[imageIdIndex]) {
-            segmentsOnFrame[imageIdIndex] = [];
-        }
-
-        if (frameHasSegments) {
-            segmentsOnFrame[imageIdIndex].push(segmentIndex);
         }
     }
 
