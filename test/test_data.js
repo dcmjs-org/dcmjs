098c89d131a5797d323b628345bf4f733274f021
jmhmd <jhostetter@gmail.com>
2020-09-07 15:24:19 -0400

    Add pattern matching for running one test from command line

--- test/test_data.js
+++ test/test_data.js
@@ -228,10 +228,15 @@ const tests = {
 }
 
 
-exports.test = async () => {
+exports.test = async (testToRun) => {
   Object.keys(tests).forEach(testName => {
+    if (testToRun && !testName.toLowerCase().includes(testToRun.toLowerCase())) {
+      console.log("-- Skipping " + testName);
+      return false;
+    }
     console.log("-- Starting " + testName);
     tests[testName]();
   });
 }
 
+exports.tests = tests;
