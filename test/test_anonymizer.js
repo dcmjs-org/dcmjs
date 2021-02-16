57182d38af48db3f082fda54db5ee719c212bb3a
Victor Parmar <vic@smalldata.tech>
2021-02-16 17:12:39 +0100

    Add anonymizer test case

--- test/test_anonymizer.js
+++ test/test_anonymizer.js
@@ -1,12 +1,35 @@
 const expect = require("chai").expect;
-const dcmjs = require("../build/dcmjs");
-
 const fs = require("fs");
+const path = require("path");
+
+const dcmjs = require("../build/dcmjs");
+const { DicomMetaDictionary, DicomDict, DicomMessage } = dcmjs.data;
+const { cleanTags } = dcmjs.anonymizer;
 
 const tests = {
   test_export: () => {
-    const cleanTags = dcmjs.anonymizer.cleanTags;
     expect(typeof cleanTags).to.equal("function");
+  },
+  test_anonymization: () => {
+    // given
+    const arrayBuffer = fs.readFileSync(path.resolve(__dirname, "sample-dicom.dcm")).buffer;
+    const dicomDict = DicomMessage.readFile(arrayBuffer);
+    expect(1).to.equal(1);
+
+    const tagInfo = dcmjs.data.DicomMetaDictionary.nameMap["PatientName"];
+    const tagNumber = tagInfo.tag,
+      tagString = dcmjs.data.Tag.fromPString(tagNumber).toCleanString();
+
+    const patientIDTag = dicomDict.dict[tagString];
+    const patientIDValue = patientIDTag.Value;
+
+    expect(patientIDValue).to.deep.equal(["Fall 3"]);
+
+    // when
+    cleanTags(dicomDict.dict);
+
+    // then
+    expect(patientIDTag.Value).to.deep.equal(["ANON^PATIENT"]);
   },
 };
 
