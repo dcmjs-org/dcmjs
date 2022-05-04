e3954b2991be3ce4dd0bb99429509cdb288380dc
Alireza <ar.sedghi@gmail.com>
2022-05-03 20:26:45 -0400

    feat: Add ArrowAnnotate adapter for cs3d

--- src/adapters/Cornerstone3D/index.js
+++ src/adapters/Cornerstone3D/index.js
@@ -2,11 +2,13 @@ import MeasurementReport from "./MeasurementReport.js";
 import Length from "./Length.js";
 import Bidirectional from "./Bidirectional.js";
 import EllipticalROI from "./EllipticalROI.js";
+import ArrowAnnotate from "./ArrowAnnotate.js";
 
 const Cornerstone3D = {
     Length,
     Bidirectional,
     EllipticalROI,
+    ArrowAnnotate,
     MeasurementReport
 };
 
