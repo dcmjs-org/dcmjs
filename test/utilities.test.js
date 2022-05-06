25d0f6e095831ab75152a44bd9a62be34ae761c0
Bill Wallace <wayfarer3130@gmail.com>
2022-05-06 11:40:32 -0400

    fix(dcmjs):Use a proxy to fix #263
    The issue was that there was an accessor being added for a very large
    integer (0x60030010), causing the array to be that size, but with a null
    value for everything up to that size.  Changed to use a proxy value
    instead, which allows differentiation by string type, and by whether the
    underlying object has the original value.

--- test/utilities.test.js
+++ test/utilities.test.js
@@ -6,13 +6,16 @@ const { addAccessors } = utilities;
 it("testAddAccessor", () => {
     const baseValue = { a: 1, b: 2 };
     const arrValue = [baseValue];
-    addAccessors(arrValue, baseValue);
-    expect(arrValue.a).toEqual(1);
+    const val = addAccessors(arrValue, baseValue);
+    expect(val.a).toEqual(1);
     baseValue.a = 3;
-    expect(arrValue.a).toEqual(3);
-    arrValue.b = 4;
+    expect(val.a).toEqual(3);
+    val.b = 4;
     expect(baseValue.b).toEqual(4);
+
+    // Check that we can iterate as an array
     const forArr = [];
-    arrValue.forEach(item => forArr.push(item));
+    val.forEach(item => forArr.push(item));
     expect(forArr.length).toEqual(1);
+    expect(forArr[0]).toEqual(baseValue);
 });
