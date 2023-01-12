3c62e11f1d116f9f6dcf89e31b23fc469a51e158
Bill Wallace <wayfarer3130@gmail.com>
2023-01-12 10:43:10 -0500

    fix:Add more utilities - compression, addAccessor, orientation

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,13 +1,17 @@
-import TID1500 from "./TID1500/index.js";
-import TID300 from "./TID300/index.js";
-import message from "./Message.js";
-import addAccessors from "./addAccessors.js";
+import TID1500 from "./TID1500";
+import TID300 from "./TID300";
+import message from "./Message";
+import addAccessors from "./addAccessors";
+import * as orientation from "./orientation";
+import * as compression from "./compression/rleSingleSamplePerPixel";
 
 const utilities = {
     TID1500,
     TID300,
     message,
-    addAccessors
+    addAccessors,
+    orientation,
+    compression
 };
 
 export default utilities;
