e84a36cb2b40a1c95132135290ec529887ad7e89
Taejin Kim <67031139+kimtaejin922@users.noreply.github.com>
2024-01-25 03:51:13 +0900

    fix(seg): compatibility issue for SEG sourcing MG images (#351)
    * added unit test for SEG sourcing MG images
    
    * fix incompatibility issue for SEG sourcing MG images

--- test/adapters.test.js
+++ test/adapters.test.js
@@ -1,1 +1,45 @@
-it("No tests yet", () => {});
+import "regenerator-runtime/runtime.js";
+import fs from "fs";
+import Segmentation_4X from "../src/adapters/Cornerstone/Segmentation_4X"
+import { getTestDataset } from "./testUtils.js";
+
+const mockMetadataProvider = {
+    get: (type, imageId) => {
+        // Unlike CT, is missing coordinate system such as frameOfReferenceUID or rowCosines attributes
+        if (imageId === "mg://1") {
+            return {
+                seriesInstanceUID: "1.2.840.113681.167838594.1562401072.4432.2070.71100000",
+                rows: 3328,
+                columns: 2560,
+                pixelSpacing: [0.065238, 0.065238],
+                rowPixelSpacing: 0.065238,
+                columnPixelSpacing: 0.065238,
+                columnCosines: null,
+                frameOfReferenceUID: undefined,
+                imageOrientationPatient: undefined,
+                imagePositionPatient: undefined,
+                rowCosines: null,
+                sliceLocation: undefined,
+                sliceThickness: undefined
+            };
+        }
+    }
+}
+
+it("Can generate tool state (4X) with SEG sourcing MG images without throwing any errors", async () => {
+    const url =
+        "https://github.com/dcmjs-org/data/releases/download/mg-seg/seg-test-SEG.dcm";
+    const dcmPath = await getTestDataset(url, "seg-test-SEG.dcm")
+    const arrayBuffer = fs.readFileSync(dcmPath).buffer
+
+    expect(
+        () => {
+            Segmentation_4X.generateToolState(
+                ["mg://1"],
+                arrayBuffer,
+                mockMetadataProvider
+            )
+        },
+    ).not.toThrowError();
+
+});
