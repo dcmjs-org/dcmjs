d38b9a0250b44bc5bf01c83bf7480635d3285fa9
Andrey Dubov <andrei.dubov98@gmail.com>
2024-03-04 09:13:10 +0300

    Fix UN vr test formatting

--- test/data.test.js
+++ test/data.test.js
@@ -1111,8 +1111,7 @@ describe("test_un_vr", () => {
         const expectedExposureIndex = 662;
         const expectedDeviationIndex = -1.835;
 
-        const url =
-            "https://github.com/dcmjs-org/data/releases/download/unknown-VR/sample-dicom-with-un-vr.dcm";
+        const url = "https://github.com/dcmjs-org/data/releases/download/unknown-VR/sample-dicom-with-un-vr.dcm";
         const dcmPath = await getTestDataset(url, "sample-dicom-with-un-vr.dcm");
 
         const file = await promisify(fs.readFile)(dcmPath);
