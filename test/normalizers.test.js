6840af9d20333144675227b7006772a3a1b84e46
Igor Šimko <simko.igor@gmail.com>
2024-10-29 20:24:28 +0100

    feat: Add OP normalizer (#403)

--- test/normalizers.test.js
+++ test/normalizers.test.js
@@ -1,1 +1,21 @@
-it("No tests yet", () => {});
+import "regenerator-runtime/runtime.js";
+
+import fs from "fs";
+import { jest } from "@jest/globals";
+import { DicomMessage } from "../src/DicomMessage";
+import { DicomMetaDictionary } from "../src/DicomMetaDictionary";
+import dcmjs from "../src";
+
+// The asset downloads in this file might take some time on a slower connection
+jest.setTimeout(60000);
+
+it("test_normalizer_op", async () => {
+    const file = fs.readFileSync('test/sample-op.dcm');
+    const dicomDict = DicomMessage.readFile(file.buffer);
+
+    const dataset = DicomMetaDictionary.naturalizeDataset(dicomDict.dict);
+    const multiframe = dcmjs.normalizers.Normalizer.normalizeToDataset([dataset]);
+
+    expect(dataset.NumberOfFrames).toEqual(1);
+    expect(multiframe.NumberOfFrames).toEqual(1);
+});
