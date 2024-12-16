8914314fbacc35667af5ca9dbcb83cf3e1377570
Igor Šimko <simko.igor@gmail.com>
2024-12-16 11:59:32 +0100

    Add OCT normalizer test

--- test/normalizers.test.js
+++ test/normalizers.test.js
@@ -2,6 +2,7 @@ import "regenerator-runtime/runtime.js";
 
 import fs from "fs";
 import { jest } from "@jest/globals";
+import { getTestDataset } from "./testUtils";
 import { DicomMessage } from "../src/DicomMessage";
 import { DicomMetaDictionary } from "../src/DicomMetaDictionary";
 import dcmjs from "../src";
@@ -10,12 +11,30 @@ import dcmjs from "../src";
 jest.setTimeout(60000);
 
 it("test_normalizer_op", async () => {
-    const file = fs.readFileSync('test/sample-op.dcm');
+    const file = fs.readFileSync("test/sample-op.dcm");
     const dicomDict = DicomMessage.readFile(file.buffer);
 
     const dataset = DicomMetaDictionary.naturalizeDataset(dicomDict.dict);
-    const multiframe = dcmjs.normalizers.Normalizer.normalizeToDataset([dataset]);
+    const multiframe = dcmjs.normalizers.Normalizer.normalizeToDataset([
+        dataset
+    ]);
 
     expect(dataset.NumberOfFrames).toEqual(1);
     expect(multiframe.NumberOfFrames).toEqual(1);
+});
+
+it("test_normalizer_oct", async () => {
+    const url =
+        "https://github.com/dcmjs-org/data/releases/download/oct/oct.dcm";
+    const dcmPath = await getTestDataset(url, "oct.dcm");
+    const file = fs.readFileSync(dcmPath);
+    const dicomDict = DicomMessage.readFile(file.buffer);
+
+    const dataset = DicomMetaDictionary.naturalizeDataset(dicomDict.dict);
+    const multiframe = dcmjs.normalizers.Normalizer.normalizeToDataset([
+        dataset
+    ]);
+
+    expect(dataset.NumberOfFrames).toEqual(97);
+    expect(multiframe.NumberOfFrames).toEqual(97);
 });
