89b74988b39f2b0d521bbd606f9f8a471d7b1b17
Luis M. Santos <lsantos@medicalmasses.com>
2026-03-06 15:07:19 -0500

    chore moving constants into their own files.
    Signed-off-by: Luis M. Santos <lsantos@medicalmasses.com>

---
+++ src/constants/sopClassUIDs.js
@@ -0,0 +1,34 @@
+// Subset of those listed at:
+// http://dicom.nema.org/medical/dicom/current/output/html/part04.html#sect_B.5
+export const sopClassNamesByUID = {
+    "1.2.840.10008.5.1.4.1.1.20": "NMImage",
+    "1.2.840.10008.5.1.4.1.1.2": "CTImage",
+    "1.2.840.10008.5.1.4.1.1.2.1": "EnhancedCTImage",
+    "1.2.840.10008.5.1.4.1.1.2.2": "LegacyConvertedEnhancedCTImage",
+    "1.2.840.10008.5.1.4.1.1.3.1": "USMultiframeImage",
+    "1.2.840.10008.5.1.4.1.1.4": "MRImage",
+    "1.2.840.10008.5.1.4.1.1.4.1": "EnhancedMRImage",
+    "1.2.840.10008.5.1.4.1.1.4.2": "MRSpectroscopy",
+    "1.2.840.10008.5.1.4.1.1.4.3": "EnhancedMRColorImage",
+    "1.2.840.10008.5.1.4.1.1.4.4": "LegacyConvertedEnhancedMRImage",
+    "1.2.840.10008.5.1.4.1.1.6.1": "USImage",
+    "1.2.840.10008.5.1.4.1.1.6.2": "EnhancedUSVolume",
+    "1.2.840.10008.5.1.4.1.1.7": "SecondaryCaptureImage",
+    "1.2.840.10008.5.1.4.1.1.30": "ParametricMapStorage",
+    "1.2.840.10008.5.1.4.1.1.66": "RawData",
+    "1.2.840.10008.5.1.4.1.1.66.1": "SpatialRegistration",
+    "1.2.840.10008.5.1.4.1.1.66.2": "SpatialFiducials",
+    "1.2.840.10008.5.1.4.1.1.66.3": "DeformableSpatialRegistration",
+    "1.2.840.10008.5.1.4.1.1.66.4": "Segmentation",
+    "1.2.840.10008.5.1.4.1.1.66.7": "LabelmapSegmentation", // Labelmap Segmentation SOP Class UID
+    "1.2.840.10008.5.1.4.1.1.67": "RealWorldValueMapping",
+    "1.2.840.10008.5.1.4.1.1.88.11": "BasicTextSR",
+    "1.2.840.10008.5.1.4.1.1.88.22": "EnhancedSR",
+    "1.2.840.10008.5.1.4.1.1.88.33": "ComprehensiveSR",
+    "1.2.840.10008.5.1.4.1.1.88.34": "Comprehensive3DSR",
+    "1.2.840.10008.5.1.4.1.1.128": "PETImage",
+    "1.2.840.10008.5.1.4.1.1.130": "EnhancedPETImage",
+    "1.2.840.10008.5.1.4.1.1.128.1": "LegacyConvertedEnhancedPETImage",
+    "1.2.840.10008.5.1.4.1.1.77.1.5.1": "OphthalmicPhotography8BitImage",
+    "1.2.840.10008.5.1.4.1.1.77.1.5.4": "OphthalmicTomographyImage"
+};
